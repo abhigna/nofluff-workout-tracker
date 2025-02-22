5707e71b3918b3d30be99736065c820ccdeae939
Abhigna Nagaraja <abhigna.n4@gmail.com>
2025-02-22 07:28:58 +0000

    Push basename fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import TrackingSheet from "./pages/TrackingSheet";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/track/:routineId" element={<TrackingSheet />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
